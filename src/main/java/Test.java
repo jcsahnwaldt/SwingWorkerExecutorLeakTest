47f4c9a150cb93c049e05789734681cebe81f47c
Christopher Sahnwaldt <jcsahnwaldt@gmail.com>
2023-08-19 03:30:05 +0200

    done

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,64 @@
+import sun.awt.SunToolkit;
 import sun.awt.AppContext;
 
+import java.awt.Toolkit;
+
+import javax.swing.SwingWorker;
+import java.lang.ref.WeakReference;
+import java.util.concurrent.ExecutorService;
+import java.beans.PropertyChangeListener;
+
 public class Test {
-  public static void main(String[] args) {
-    var appContext = AppContext.getAppContext();
-    System.out.println(appContext);
+
+  private static AppContext appContext;
+
+  public static void main(String[] args) throws Exception {
+
+    // AppContext must be in new thread group, otherwise dispose() throws
+    Thread thread = new Thread(new ThreadGroup("Test"), "Test") {
+      public void run() {
+        appContext = ((SunToolkit)Toolkit.getDefaultToolkit()).createNewAppContext();
+        new SwingWorker<Void, Void>() {
+          protected Void doInBackground() {
+            return null;
+          }
+        }.execute(); // calls SwingWorker.getWorkersExecutorService()
+      }
+    };
+    thread.start();
+    thread.join();
+
+    // SwingWorker.getWorkersExecutorService() stored the executor in the AppContext map
+    WeakReference<ExecutorService> executor = new WeakReference<>((ExecutorService)appContext.get(SwingWorker.class));
+
+    appContext.dispose();
+
+    // dispose() cleared the AppContext map
+    if (appContext.get(SwingWorker.class) != null) throw new AssertionError();
+
+    // dispose() called the listener defined in SwingWorker.getWorkersExecutorService(),
+    // which called shutdown() on the executor
+    if (! executor.get().isShutdown()) throw new AssertionError();
+
+    // but the listener retains a strong reference to the executor,
+    // and dispose() doesn't clear the list of listeners
+    // reference chain: appContext -> listener -> executor
+
+    if (false) {
+      // this would make the executor unreachable
+      appContext = null;
+    }
+
+    if (false) {
+      // this would make the executor unreachable
+      PropertyChangeListener listener = appContext.getPropertyChangeListeners(AppContext.DISPOSED_PROPERTY_NAME)[0];
+      appContext.removePropertyChangeListener(AppContext.DISPOSED_PROPERTY_NAME, listener);
+      listener = null;
+    }
+
+    System.gc();
+    if (executor.get() != null) {
+      throw new RuntimeException("executor created by SwingWorker.getWorkersExecutorService() is still strongly referenced");
+    }
   }
 }
